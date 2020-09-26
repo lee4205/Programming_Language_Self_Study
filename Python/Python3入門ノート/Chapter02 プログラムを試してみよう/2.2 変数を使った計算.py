a9d0518ab842f549e541751b67ce7c73eb30591d
lee4205 <leejinyien4205@gmail.com>
2020-09-26 22:10:23 +0900

    変数を用いて計算する

---
+++ Python/Python3入門ノート/Chapter02 プログラムを試してみよう/2.2 変数を使った計算.py
@@ -0,0 +1,24 @@
+# 変数に値を代入
+
+kosu = 12 * 5
+kosu
+
+red_ball = 5
+white_ball = 7
+all = red_ball + white_ball
+all
+
+red_ball / all
+
+red_ball = 3
+all
+
+all = red_ball + white_ball
+all
+
+'''
+redball
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+NameError: name 'redball' is not defined
+'''
